5c66e8d7837acdc0bdd222012aeb03a03b3af294
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-06-17 18:57:54 +0200

    2_1-state-TodoList_1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-import ColorPicker from "./ColorPicker/ColorPicker";
+import React, { Component } from "react";
+// import ColorPicker from "./ColorPicker";
 // import Counter from './Counter/Counter'
 // import Dropdown from "./Dropdown/Dropdown";
+import TodoList from './TodoList';
 
+class App extends Component {
 
-const App = () => {
-  return (
-    <>
-    {/* <Dropdown /> */}
-    <ColorPicker options={colorPickerOptions} />
-    {/* <Counter /> */}
-    </>
-  );
-};
+  state = {
+    todos: [
+      { id: 'id-1', text: 'Выучить основы React', completed: false },
+      { id: 'id-2', text: 'Разобраться с React Route', completed: false },
+      { id: 'id-3', text: 'Понять Redux', completed: false },
+    ],
+  };
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+  render() {
+    const { todos } = this.state;
+    
+    return (
+      <>
+      <h1>hi</h1>
+      {/* <Dropdown /> */}
+      {/* <ColorPicker options={colorPickerOptions} /> */}
+      {/* <Counter /> */}
+      <TodoList todos= {todos}/>
+      </>
+
+    )
+  }
+}
+
+// const App = () => {
+//   return (
+   
+//   );
+// };
+
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 export default App;
